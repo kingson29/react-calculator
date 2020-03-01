706a6ce17f56d4af0afe09f38d4e87097ea05d32
kingson29 <50691571+kingson29@users.noreply.github.com>
2020-03-02 02:18:09 +0800

    add logic in calculator + - x / integer

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -4,14 +4,61 @@ import Displayer from "./displayer";
 
 class Calculator extends Component {
   state = {
-    symbols: [1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "x", 0, ".", "=", "/"]
+    symbols: [1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "x", 0, ".", "=", "/"],
+    preOperator: "",
+    operator: "",
+    n1: "0",
+    n2: "0",
+    result: 0
   };
+  calculateResult(n1_input, preOperator, n2_input) {
+    let n1 = parseFloat(n1_input);
+    let n2 = parseFloat(n2_input);
+    console.log(n1, preOperator, n2);
+    if (preOperator === "+") return n1 + n2;
+    if (preOperator === "-") return n1 - n2;
+    if (preOperator === "x") return n1 * n2;
+    if (preOperator === "/") return n1 / n2;
+  }
 
   handleClick = action => {
-    if (typeof action === "number") console.log("number key");
-    if (["+", "-", "x", "/"].includes(action)) console.log("operator key");
-    if (action === "=") console.log("equal key");
-    if (action === ".") console.log("decimal key");
+    const { preOperator, operator, n1, n2, result } = this.state;
+    let previousOperator = preOperator;
+    let currentOperator = operator;
+    let currentN1 = n1;
+    let currentN2 = n2;
+    let currentResult = result;
+    if (action === "=") {
+      currentResult = this.calculateResult(
+        currentN1,
+        previousOperator,
+        currentN2
+      );
+    }
+    if (["+", "-", "x", "/"].includes(action)) {
+      //if the current operation is "operator"
+      currentOperator = action;
+      currentN1 = currentN2;
+      currentN2 = 0; // shift n2 to n1 and set n2 to 0
+    }
+    if (typeof action === "number") {
+      //if the current operation is "number"
+      if (["+", "-", "x", "/"].includes(currentOperator)) {
+        //if there exist a previous operator
+        previousOperator = currentOperator;
+        currentOperator = ""; //reset operator
+        currentN2 = action.toString();
+      } else {
+        currentN2 += action.toString();
+      }
+    }
+    this.setState({
+      preOperator: previousOperator,
+      operator: currentOperator,
+      n1: currentN1,
+      n2: currentN2,
+      result: currentResult
+    });
   };
 
   render() {
