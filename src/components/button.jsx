061e811c0a34124d2af90bcb8b2cb43d9e53e1e6
kingson29 <50691571+kingson29@users.noreply.github.com>
2020-03-02 00:28:19 +0800

    add and style the button

---
+++ src/components/button.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+const Button = () => {
+  return <div className="calbutton">Symbol</div>;
+};
+
+export default Button;
--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
+import Button from "./button";
 
 class Buttons extends Component {
   state = {};
   render() {
-    return <div class="button-area"></div>;
+    return (
+      <div class="button-area">
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+    );
   }
 }
 
